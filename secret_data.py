d3c11622858695fadcc198692dfad0a33a7d6cf3
Your Name <you@example.com>
2017-11-13 11:51:12 -0500

    initial commit

---
+++ secret_data.py
@@ -0,0 +1,4 @@
+# Download and rename this file to secret_data.py!
+
+client_key = "svAekOzTKOQOvnoSEkY9R6Z4UwXWujihMTE47GnjQyjtTpjItO"
+client_secret = "n4ozM533VLnfzOXVheS40pNOgLa1suXh4dtcHDhYBdgNs7OReB"

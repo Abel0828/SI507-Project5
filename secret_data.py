24156c191e5f0171cdd63dbca3f6b72d1859eab8
Abel0828 <ywangdr@connect.ust.hk>
2017-11-13 11:52:12 -0500

    Delete secret_data.py

--- secret_data.py
+++
@@ -1,4 +0,0 @@
-# Download and rename this file to secret_data.py!
-
-client_key = "svAekOzTKOQOvnoSEkY9R6Z4UwXWujihMTE47GnjQyjtTpjItO"
-client_secret = "n4ozM533VLnfzOXVheS40pNOgLa1suXh4dtcHDhYBdgNs7OReB"
